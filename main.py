dccc0df6bf28978209b6a356ec647461988b0750
Jonas Thomsen <jonastoftethomsen@gmail.com>
2022-03-17 21:10:59 +0100

    Add Prometheus server

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
 from scraper import scraper
+from prometheus_client import start_http_server, Gauge
+import time
+import os
 
-print("latest value scraped from the simulator: " + str(scraper.getLatest()))
+PORT = os.environ.get('PORT')
+if (PORT == None):
+    PORT = 8080
 
-print("latest errors scraped from the simulator: " + str(scraper.getErrors()))
+if __name__ == '__main__':
+
+    start_http_server(PORT)
+
+    latest = Gauge('latest', 'Latest value from the simulator')
+    connectionError = Gauge('group_d_connection_error', 'Current ConnectionError value from the simulator')
+    follow = Gauge('group_d_follow', 'Current Follow error value from the simulator')
+    tweet = Gauge('group_d_tweet', 'Current Tweet error value from the simulator')
+    unfollow = Gauge('group_d_unfollow', 'Current Unfollow error value from the simulator')
+    readTimeout = Gauge('group_d_readTimeout', 'Current ReadTimeout error value from the simulator')
+    register = Gauge('group_d_register', 'Current Register error value from the simulator')
+
+    while True:
+        latest.set(scraper.getLatest())
+        errors = scraper.getErrors()
+        connectionError.set(errors['ConnectionError'])
+        follow.set(errors['follow'])
+        tweet.set(errors['tweet'])
+        unfollow.set(errors['unfollow'])
+        readTimeout.set(errors['ReadTimeout'])
+        register.set(errors['register'])
+
+        time.sleep(10)
